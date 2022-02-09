833bde848c6c24d663e26c9ac8add98181c13979
scraggy001 <99342440+Karl002@users.noreply.github.com>
2022-02-09 23:47:12 +0800

    OOP

---
+++ app/Controllers/ProfileController.php
@@ -0,0 +1,21 @@
+<?php
+// app/Controllers/ProfileController.php
+
+namespace App\Controllers;
+use CodeIgniter\Controller;
+
+
+class ProfileController extends BaseController
+{
+    public function index()
+    {
+
+        helper(['form']);
+        $data = [];
+        echo view('profile', $data);
+        $session = session();
+        echo "Hello  : ".$session->get('name');
+        
+        
+    }
+}
---
+++ app/Views/profile.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Profile </title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous"></head>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<body>
+
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <ul class="navbar-nav">
+    <li class="nav-item active">
+      <a class="nav-link" href="<?php echo base_url('ProfileController');?>"> <button type="button" class="btn btn-primary">Home</button></a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="<?php echo base_url('UserCrud');?>"> <button type="button" class="btn btn-secondary">CRUD</button></a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="<?php echo base_url('SigninController');?>"> <button type="button" class="btn btn-secondary">LogOut</button></a>
+    </li>
+    <li class="nav-item">
+    </li>
+  </ul>
+  
+</nav>
+<div class="container">
+        <div class="row justify-content-md-center">
+            <div class="col-5">`
+
+              
+</body>
+  
+</html>
+
+
